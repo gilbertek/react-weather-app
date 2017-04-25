3771714552974895dced325a721775cb37a067de
Gilbert F. Sewovoe-Ekoue <gilberts55@hotmail.com>
2017-04-24 22:40:30 -0400

    Add component

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'React';
+import ZipForm from './ZipForm';
+
+class App extends React.Component {
+  render () {
+    return <div className="app">
+      <ZipForm />
+    </div>;
+  }
+}
+
+export default App;
---
+++ src/components/ZipForm.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+
+class ZipForm extends Component {
+  render() {
+    return (
+      <div className='zip-form'>
+        <form>
+          <label htmlFor='zipcode'>Zip Code</label>
+          <input
+            className='form-control'
+            type='input'
+            name='zipcode' />
+          <button
+            type='submit'
+            className='btn btn-success'>
+            Get Forecast!</button>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default ZipForm;
