4226ac39e1614f30c23a5538e64710580092d54f
Gilbert F. Sewovoe-Ekoue <gilberts55@hotmail.com>
2017-04-26 20:22:14 -0400

    Weather components

---
+++ src/components/WeatherList.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'React, { Component }';
+
+class WeatherList extends Component {
+
+  render() {
+    const { days } = this.props;
+
+    return (
+      <div className="weather-list flex-parent">
+
+      </div>
+    );
+  }
+}
