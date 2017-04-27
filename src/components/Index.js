0329ed1e8730c4cdc1e93f028d9b972d0537a220
Gilbert F. Sewovoe-Ekoue <gilberts55@hotmail.com>
2017-04-26 20:22:51 -0400

    update conf

--- src/components/Index.js
+++ src/components/Index.js
@@ -1,10 +1,39 @@
 import React from 'React';
 import ZipForm from './ZipForm';
+import { get } from 'axios';
 
 class Index extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      zipcode: '',
+      city: {},
+      dates: [],
+      selectedDate: null
+    };
+
+
+    this.onFormSubmit = this.onFormSubmit.bind(this);
+  }
+
+  onFormSubmit(zipcode) {
+    get(`http://localhost:3004/weather/${zipcode}`)
+      .then(({ data }) => {
+        const { city, list: dates } = data;
+
+        this.setState({
+          zipcode,
+          city,
+          dates,
+          selectedDate: null
+        });
+    });
+  }
+
   render () {
     return <div className="app">
-      <ZipForm />
+      <ZipForm onSubmit={this.onFormSubmit} />
     </div>;
   }
 }
